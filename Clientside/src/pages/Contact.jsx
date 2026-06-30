1da8ee99341dd17276dbd18039bd667f93c9aa4e
NikhilDarekar <nikhilx97@gmail.com>
2026-06-30 15:47:27 +0530

    .

--- Clientside/src/pages/Contact.jsx
+++ Clientside/src/pages/Contact.jsx
@@ -10,8 +10,29 @@ const Contact = () => {
   }, [])
 
   return (
-    <div>
-        
+    <div className='min-h-screen bg-gray-900 text-white'>
+      <p>Hi, this is contact us page</p>
+        <form className='bg-white mb-2 w-2xl'>
+          <div className='w-full p-2 border rounded-xl'>
+            <label>Name</label>
+            <input 
+              type='text'
+              id='name'
+              placeholder='Enter your name'
+              className='w-xl text-gray-800 p-2 border gap-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400'
+              />
+
+            <label>Email</label>
+            <input 
+              type='text'
+              id='name'
+              placeholder='Enter your Email'
+              className='w-xl text-gray-800 p-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400'
+              />
+
+          </div>
+
+        </form>
     </div>
   )
 }
